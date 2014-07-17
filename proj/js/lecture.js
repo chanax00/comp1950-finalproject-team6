d503b8925f9874826d285e6aaad3cccd3c102879
calvinjun <calvinscjun@hotmail.com>
2014-07-17 11:51:10 -0700

    almost finished the lecture page

--- proj/js/lecture.js
+++ proj/js/lecture.js
@@ -22,31 +22,233 @@ function getCookie(cname)
 	return "";
 }
 
+function readCookie(name)
+{
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++)
+	{
+		var c = ca[i];
+		while (c.charAt(0)==' ')
+		{
+			c = c.substring(1,c.length);
+		}
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function createCookie(name,value,days)
+{
+	if (days)
+	{
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires;
+}
+
+function selectCard()
+{
+	var card = readCookie("card");
+	if (card == null)
+	{
+		$("#card_list ul li:nth-child(1)").css("color","yellow");
+		var cardName = $($("#card_list ul li:nth-child(1)")).html();
+		createCookie("card",cardName,365);
+	}
+	else
+	{
+		var iCard = 1;
+		while (iCard > 0)
+		{
+			var nthChild = $("#card_list ul li:nth-child("+String(iCard)+")");
+			if (nthChild.css('color') == null) break;
+			if (nthChild.html() == card)
+			{
+				nthChild.css('color','yellow');
+			}
+			else nthChild.css('color','#dedede');
+			iCard++;
+		}
+	}
+}
+
+function initCards()
+{
+	var strContent = document.getElementById("content").innerHTML;
+	var strList = "<ul>";
+	var iStr0 = strContent.search("<h2");
+	strContent = strContent.slice(iStr0+3,strContent.length);
+	iStr0 = strContent.search('>') + 1;
+	var iCard = 1;
+	while (iStr0 != -1 && strContent.length > 0)
+	{
+		var iStr1 = strContent.search("</h2>");
+		if (iStr1 == -1) break;
+		var cardName = strContent.slice(iStr0,iStr1);
+		strList += "<li>"+cardName+"</li>";
+		strContent = strContent.slice(iStr1+5,strContent.length);
+		iStr0 = strContent.search("<h2");
+		strContent = strContent.slice(iStr0+3,strContent.length);
+		iStr0 = strContent.search('>') + 1;
+		iCard++;
+	}
+	document.getElementById("card_list").innerHTML = strList+"</ul>";
+	$(".dark_panel").css("height",$("#card_list").css("height"));
+	$("#card_list").css("display","block");
+	$("#session_list").css("display","none");
+	selectCard();
+	showCard();
+	$("#card_list ul li").click(function(){
+		var iCard = 1;
+		while (iCard > 0)
+		{
+			var nthChild = $("#card_list ul li:nth-child("+String(iCard)+")");
+			if (nthChild.css("color") == null) break;
+			if (nthChild.html() == readCookie('card'))
+			{
+				nthChild.css('color','#dedede');
+			}
+			iCard++;
+		}
+		hideCard();
+		var cardName = $(this).html();
+		createCookie("card",cardName,365);
+		$(this).css("color","yellow");
+		showCard();
+	});
+}
+
+function clickOption(option)
+{
+	document.cookie = "option = " + option;
+	switch (option)
+	{
+		case 'sessions':
+			$("#session_list").css("display","block");
+			$("#card_list").css("display","none");
+			$(".dark_panel").css("height",$("#session_list").css("height"));
+			break;
+		case 'cards':
+			initCards();
+			$("#session_list").css("display","none");
+			$("#card_list").css("display","block");
+			$(".dark_panel").css("height",$("#card_list").css("height"));
+			selectCard();
+			showCard();
+			break;
+	}
+}
+
+function showCard()
+{
+	var iCard = 1;
+	while (iCard > 0)
+	{
+		var nthChild = $("h2:nth-child("+String(iCard)+")");
+		if (nthChild.html() == "") break;
+		if (nthChild.html() == readCookie('card'))
+		{
+			$("h2:nth-child("+String(iCard)+")").css('display','block');
+			$("h2:nth-child("+String(iCard)+")+div").css('display','block');
+			return;
+		}
+		iCard++;
+	}
+}
+
+function hideCard()
+{
+	var iCard = 1;
+	while (iCard > 0)
+	{
+		var nthChild = $("h2:nth-child("+String(iCard)+")");
+		if (nthChild.html() == "") break;
+		if (nthChild.html() == readCookie('card'))
+		{
+			$("h2:nth-child("+String(iCard)+")").css('display','none');
+			$("h2:nth-child("+String(iCard)+")+div").css('display','none');
+			return;
+		}
+		iCard++;
+	}
+}
+
+function showPrevCard()
+{
+	var iCard = 1;
+	while (iCard > 0)
+	{
+		var nthChild = $("h2:nth-child("+String(iCard)+")");
+		if (nthChild.html() == "") break;
+		if (nthChild.html() == readCookie('card'))
+		{
+			if (iCard - 2 > 0)
+			{
+				$("h2:nth-child("+String(iCard)+")").css('display','none');
+				$("h2:nth-child("+String(iCard)+")+div").css('display','none');
+				iCard-=2;
+				$("h2:nth-child("+String(iCard)+")").css('display','block');
+				$("h2:nth-child("+String(iCard)+")+div").css('display','block');
+				createCookie("card",$("h2:nth-child("+String(iCard)+")").html(),365);
+				selectCard();
+			}
+			return;
+		}
+		iCard++;
+	}
+}
+
+function showNextCard()
+{
+	var iCard = 1;
+	while (iCard > 0)
+	{
+		var nthChild = $("h2:nth-child("+String(iCard)+")");
+		if (nthChild.html() == "") break;
+		if (nthChild.html() == readCookie('card'))
+		{
+			if ($("h2:nth-child("+String(iCard+2)+")").css("color") != null)
+			{
+				$("h2:nth-child("+String(iCard)+")").css('display','none');
+				$("h2:nth-child("+String(iCard)+")+div").css('display','none');
+				iCard+=2;
+				$("h2:nth-child("+String(iCard)+")").css('display','block');
+				$("h2:nth-child("+String(iCard)+")+div").css('display','block');
+				createCookie("card",$("h2:nth-child("+String(iCard)+")").html(),365);
+				selectCard();
+			}
+			return;
+		}
+		iCard++;
+	}
+}
+
+function showPanel()
+{
+	$(".dark_panel").css("height",$("#session_list").css("height"));
+	switch (readCookie('option'))
+	{
+		default: case 'sessions':
+			$(".dark_panel").css("height",$("#session_list").css("height"));
+			$("#session_list").css("display","block");
+			$("#card_list").css("display","none");
+			break;
+		case 'cards':
+			initCards();
+			break;
+	}
+}
+
 $(document).ready(function(){
-	var pageNum = 1;
-	var page = getCookie('page');
-	if (page != '') pageNum = Number(page);
-	$(".page:nth-child("+pageNum+")").css("display","block");
 	$("#arrow_left").click(function(){
-		var pageNum = 1;
-		var page = getCookie('page');
-		if (page != '') pageNum = Number(page);
-		if (pageNum <= 1) return;
-		$(".page:nth-child("+pageNum+")").css("display","none");
-		pageNum--;
-		$(".page:nth-child("+pageNum+")").css("display","block");
-		document.cookie = "page = " + pageNum;
+		showPrevCard();
 	});
 	$("#arrow_right").click(function(){
-		var pageNum = 1;
-		var page = getCookie('page');
-		if (page != '') pageNum = Number(page);
-		pageNum++;
-		if ($(".page:nth-child("+pageNum+")").css("display") == null) return;
-		pageNum--;
-		$(".page:nth-child("+pageNum+")").css("display","none");
-		pageNum++;
-		$(".page:nth-child("+pageNum+")").css("display","block");
-		document.cookie = "page = " + pageNum;
+		showNextCard();
 	});
+	showPanel();
 })
--- proj/lecture.php
+++ proj/lecture.php
@@ -17,18 +17,51 @@
 		<script type = "text/javascript" src = "./js/lecture.js"></script>
 	</head>
 	<body>
-		<nav id = "main_nav">
-			<div id = "main_nav_home"><div></div></div>
-			<div id = "main_nav_lecture"><div></div></div>
-			<div id = "main_nav_practice"><div></div></div>
-			<div id = "main_nav_quiz"><div></div></div>			
-		</nav>
-		<section>
-			<article>
-				<?php include('./lecture_content/session01.html'); ?>
-				<div id = "arrow_right"></div>
-				<div id = "arrow_left"></div>
-			</article>
-		</section>
+		<div class = 'body'>
+			<section>
+				<article>
+					<div id = "content">
+					<?php include('./lecture_content/session01.html'); ?>
+					</div>
+					<div id = "arrow_right"></div>
+					<div id = "arrow_left"></div>
+				</article>
+			</section>
+			<section>
+				<div class = "dark_panel"></div>
+				<div id = "session_list">
+					<ol>
+						<li>tools and standards</li>
+						<li>html5</li>
+						<li>css technique</li>
+						<li>css layouts</li>
+						<li>responsive css</li>
+						<li>css3</li>
+						<li>javascript frameworks</li>
+						<li>dynamic content</li>
+						<li>templating, seo</li>
+						<li>methodology</li>
+						<li>project work</li>
+					</ol>
+				</div>
+				<div id = "card_list">
+				</div>
+			</section>
+		</div>
+		<div id = 'top_panel'></div>
+		<div class = 'body'>
+			<nav id = "main_nav">
+				<div id = "main_nav_home"><div></div></div>
+				<div id = "main_nav_lecture"><div></div></div>
+				<div id = "main_nav_practice"><div></div></div>
+				<div id = "main_nav_quiz"><div></div></div>
+			</nav>
+			<div id = "option_icons">
+				<div id = "icon_sessions" class = "option_icon" onclick = "clickOption('sessions')"></div>
+				<div id = "icon_cards" class = "option_icon" onclick = "clickOption('cards')"></div>
+				<div id = "icon_hyperlinks" class = "option_icon" onclick = "clickOption('hyperlinks')"></div>
+				<div id = "icon_customize" class = "option_icon" onclick = "clickOption('customize')"></div>
+			</div>
+		</div>
 	</body>
 </html>
